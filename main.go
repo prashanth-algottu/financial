9180d792be1d01241f571f087a703254699837ff
chinnu <algottuprashanth@gmail.com>
2024-07-25 23:06:04 +0530

    removed DB

--- main.go
+++ main.go
@@ -43,17 +43,19 @@ func addUserHandler(conn *pgx.Conn) func(w http.ResponseWriter, r *http.Request)
 }
 
 func main() {
-	ctx := context.Background()
+	// ctx := context.Background()
 	// Initialize the database connection
-	conn, err := connectDB(ctx)
-	if err != nil {
-		fmt.Printf("Error connecting to the database: %v\n", err)
-		return
-	}
-	defer conn.Close(ctx)
+	// conn, err := connectDB(ctx)
+	// if err != nil {
+	// 	fmt.Printf("Error connecting to the database: %v\n", err)
+	// 	return
+	// }
+	// defer conn.Close(ctx)
+
 	// Start the HTTP server
 	// http.HandleFunc("/", helloHandler)
-	http.HandleFunc("/add-user", addUserHandler(conn))
+	// http.HandleFunc("/add-user", addUserHandler(conn))
+	http.HandleFunc("/", simpleMessageHandler)
 
 	fmt.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8081", nil); err != nil {
@@ -61,23 +63,27 @@ func main() {
 	}
 }
 
-// connectDB connects to the PostgreSQL database and returns the connection
-func connectDB(ctx context.Context) (*pgx.Conn, error) {
-	print("1. started")
-	connStr := "postgres://postgres:chinnu@localhost:5433/postgres"
-	print("2. started")
-
-	if connStr == "" {
-		print("3. started")
-
-		return nil, fmt.Errorf("DATABASE_URL environment variable is not set")
-	}
-	print("4. started")
-
-	conn, err := pgx.Connect(ctx, connStr)
-	if err != nil {
-		return nil, fmt.Errorf("unable to connect to database: %v", err)
-	}
-
-	return conn, nil
+func simpleMessageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Prashanth")
 }
+
+// connectDB connects to the PostgreSQL database and returns the connection
+// func connectDB(ctx context.Context) (*pgx.Conn, error) {
+// 	print("1. started")
+// 	connStr := "postgres://postgres:chinnu@localhost:5433/postgres"
+// 	print("2. started")
+
+// 	if connStr == "" {
+// 		print("3. started")
+
+// 		return nil, fmt.Errorf("DATABASE_URL environment variable is not set")
+// 	}
+// 	print("4. started")
+
+// 	conn, err := pgx.Connect(ctx, connStr)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("unable to connect to database: %v", err)
+// 	}
+
+// 	return conn, nil
+// }
